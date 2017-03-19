89112def31f754a405c3142861381303ea718b98
grrrben <grrrben@gmail.com>
2017-03-19 14:24:08 +0100

    QR code reading example

--- app/src/main/java/com/atog/grrrben/kemepo/MainActivity.java
+++ app/src/main/java/com/atog/grrrben/kemepo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.atog.grrrben.kemepo;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -37,8 +39,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent barcodeAct = new Intent(MainActivity.this, BarcodeActivity.class);
+                startActivity(barcodeAct);
+                finish();
             }
         });
     }
